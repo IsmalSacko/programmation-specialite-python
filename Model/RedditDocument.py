667bb1e4b69fdc8cf2b22314e498ffe7c3678ca9
mahamoud2020 <mhoud.isman@gmail.com>
2024-01-07 07:00:54 +0100

    update RedditDocument.py

--- Model/RedditDocument.py
+++ Model/RedditDocument.py
@@ -1,8 +1,9 @@
 from Model.Document import Document
 
+# classe fille permettant de modéliser un Document Reddit
 
 class RedditDocument(Document):
-    
+    # constructeur
     def __init__(self, titre, auteur, date, url, texte):
         Document.__init__(self, titre, auteur, date, url, texte)
         self.setType("Reddit")
